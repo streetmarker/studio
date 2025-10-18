2834352182eadae694b5d91f8c33e326490812f6
Michał Jaroń <mjaron4@gmail.com>
2025-10-18 19:55:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8044418253-578b4",
+  "appId": "1:462889273731:web:7f0c14d052d18adea40eb6",
+  "apiKey": "AIzaSyCiO-PY0G6VXwz6WOI-tYVscpzHIPSj-0s",
+  "authDomain": "studio-8044418253-578b4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "462889273731"
+};
